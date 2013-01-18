25bc1f1b1fadd06802ab6f8ace66c726d1f7a78e
?ngel ?lvarez <angel@nan-tic.com>
2013-01-18 16:44:29 +0100

    [FIX] encoding

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 #This file is part account_search_with_dot module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
