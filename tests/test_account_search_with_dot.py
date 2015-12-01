b3da690fed46be18d232f719cdd4321a86c96bd4
resteve <resteve@zikzakmedia.com>
2015-12-01 11:02:33 +0100

    Tests

--- tests/test_account_search_with_dot.py
+++ tests/test_account_search_with_dot.py
@@ -1,52 +1,18 @@
-#!/usr/bin/env python
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+# This file is part of the account_search_with_dot module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountSearchWithDotTestCase(unittest.TestCase):
+class AccountSearchWithDotTestCase(ModuleTestCase):
     'Test Account Search With Dot module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_search_with_dot')
-        self.account = POOL.get('account.account')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
-    def test0010search_with_dot(self):
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            receivable, = self.account.search([
-                    ('kind', '=', 'receivable'),
-                    ])
-            receivable.code = '43000001'
-            receivable.save()
-            payable, = self.account.search([
-                    ('kind', '=', 'payable'),
-                    ])
-            payable.code = '41000001'
-            payable.save()
-
-            for field in ('code', 'rec_name'):
-                account, = self.account.search([(field, 'ilike', '43.1')])
-                self.assertEqual(account.code, u'43000001')
-                account, = self.account.search([(field, 'ilike', '41.1')])
-                self.assertEqual(account.code, u'41000001')
+    module = 'account_search_with_dot'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.account.tests import test_account
-    for test in test_account.suite():
-        if test not in suite and not isinstance(test, doctest.DocTestCase):
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountSearchWithDotTestCase))
     return suite
