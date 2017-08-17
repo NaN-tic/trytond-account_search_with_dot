0baf0986562ef13c255f96dbaa66a64d7341f9a1
Albert Cervera i Areny <albert@nan-tic.com>
2017-08-17 22:59:22 +0200

    Fix searching on rec_name when the domain starts with an 'AND' or 'OR' operator.
    Searching using the dot was implemented on the search() for both rec_name and
    code. The problem is that the implementation does not consider the case when
    the first argument contains an 'AND' or 'OR' operator.
    
    When the user searches using the dot on a list view, the client prepends the
    domain with 'AND' so the search did not work in that case.
    
    The problem has been solved with:
    
    - Implementing search_rec_name for the case of the 'rec_name' field.
    - As the code field is not a function one, it has been implemented by
      overriding search_domain() and processing all the domain tree.

--- account.py
+++ account.py
@@ -6,6 +6,7 @@ import re
 from sql import Literal
 from sql.operators import BinaryOperator, Like
 
+from trytond.const import OPERATORS
 from trytond.backend import name as backend_name
 from trytond.transaction import Transaction
 from trytond.pool import PoolMeta
@@ -37,48 +38,64 @@ class CodeWithDotMixin:
     _dot_fields = ['code']
 
     @classmethod
-    def search(cls, args, offset=0, limit=None, order=None, count=False,
-            query=False):
-        """Improves the search of accounts using a dot to
-        fill the zeroes (like 43.27 to search account
-        43000027)
-        """
-        args = args[:]
-        pos = 0
-        while pos < len(args):
-            if not args[pos]:
-                pos += 1
-                continue
-            if (args[pos][0] in ('code', 'rec_name')
-                    and args[pos][1] in ('like', 'ilike',
-                    'not like', 'not ilike') and args[pos][2]):
-                q = args[pos][2].replace('%', '')
-                if '.' in q:
-                    q = q.partition('.')
-                    regexp = regexp_function()
-                    table = cls.__table__()
-                    where = cls.get_dot_extra_where(table)
-                    if regexp:
-                        expression = '^%s0+%s$' % (q[0], q[2])
-                        ids = table.select(table.id, where=(where &
-                                regexp(table.code, expression)))
-                    else:
-                        cursor = Transaction().connection.cursor()
-                        cursor.execute(*table.select(table.id,
-                                table.code, where=(where &
-                                    Like(table.code, q[0] + '%' + q[2]))))
-                        pattern = '^%s0+%s$' % (q[0], q[2])
-                        ids = []
-                        for record in cursor.fetchall():
-                            if re.search(pattern, record[1]):
-                                ids.append(record[0])
-                    if args[pos][1].startswith('not'):
-                        args[pos] = ('id', 'not in', ids)
-                    else:
-                        args[pos] = ('id', 'in', ids)
-            pos += 1
-        return super(CodeWithDotMixin, cls).search(args, offset=offset,
-            limit=limit, order=order, count=count, query=query)
+    def get_ids(cls, q):
+        q = q.partition('.')
+        regexp = regexp_function()
+        table = cls.__table__()
+        where = cls.get_dot_extra_where(table)
+        if regexp:
+            expression = '^%s0+%s$' % (q[0], q[2])
+            ids = table.select(table.id, where=(where &
+                    regexp(table.code, expression)))
+        else:
+            cursor = Transaction().connection.cursor()
+            cursor.execute(*table.select(table.id,
+                    table.code, where=(where &
+                        Like(table.code, q[0] + '%' + q[2]))))
+            pattern = '^%s0+%s$' % (q[0], q[2])
+            ids = []
+            for record in cursor.fetchall():
+                if re.search(pattern, record[1]):
+                    ids.append(record[0])
+        return ids
+
+    @classmethod
+    def get_clause(cls, clause):
+        clause = clause
+        q = clause[2].replace('%', '')
+        if '.' in q:
+            ids = cls.get_ids(q)
+            if clause[1].startswith('not'):
+                return ('id', 'not in', ids)
+            else:
+                return ('id', 'in', ids)
+
+    @classmethod
+    def search_domain(cls, domain, active_test=True, tables=None):
+        '''
+        Improves the search of accounts using a dot to fill the zeroes (like
+        43.27 to search account 43000027)
+        '''
+        def is_leaf(expression):
+            return (isinstance(expression, (list, tuple))
+                and len(expression) > 2
+                and isinstance(expression[1], basestring)
+                and expression[1] in OPERATORS)  # TODO remove OPERATORS test
+
+        def convert_domain(domain):
+            'Replace missing product field by the MoveLine one'
+            if is_leaf(domain):
+                if domain[0] in 'code' and 'like' in domain[1] and domain[2]:
+                    clause = cls.get_clause(domain)
+                    if clause:
+                        return clause
+                return domain
+            elif domain in ['OR', 'AND']:
+                return domain
+            else:
+                return [convert_domain(d) for d in domain]
+        return super(CodeWithDotMixin, cls).search_domain(convert_domain(domain),
+            active_test=active_test, tables=tables)
 
     @classmethod
     def get_dot_extra_where(cls, table):
@@ -91,6 +108,22 @@ class Account(CodeWithDotMixin):
     @classmethod
     def get_dot_extra_where(cls, table):
         return (table.kind != 'view')
+
+    @classmethod
+    def search_rec_name(cls, name, clause):
+        '''
+        Improves the search_rec_name allowing the use of a dot to fill the
+        zeroes (like 43.27 to search account 43000027)
+        '''
+        res = super(CodeWithDotMixin, cls).search_rec_name(name, clause)
+        if 'like' in clause[1]:
+            new_clause = cls.get_clause(clause)
+            if new_clause:
+                if 'not' in new_clause[1]:
+                    return ['AND', [new_clause], res]
+                else:
+                    return ['OR', [new_clause], res]
+        return res
 
 
 class GeneralLedgerAccount(CodeWithDotMixin):
