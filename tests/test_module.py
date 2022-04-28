dbf8663f8ea19284c640d06327753fc3a9abdf91
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-28 13:15:21 +0200

    Replace test setuptools command by unittest discover

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,17 +1,17 @@
-# This file is part of the account_search_with_dot module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-import unittest
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
 from trytond.pool import Pool
-import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 
-from trytond.modules.company.tests import create_company, set_company
+from trytond.modules.company.tests import (CompanyTestMixin, create_company,
+    set_company)
 from trytond.modules.account.tests import create_chart
 
 
-class AccountSearchWithDotTestCase(ModuleTestCase):
-    'Test Account Search With Dot module'
+class AccountSearchWithDotTestCase(CompanyTestMixin, ModuleTestCase):
+    'Test AccountSearchWithDot module'
     module = 'account_search_with_dot'
 
     @with_transaction()
@@ -43,8 +43,4 @@ class AccountSearchWithDotTestCase(ModuleTestCase):
                 self.assertEqual(account.code, '41000001')
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountSearchWithDotTestCase))
-    return suite
+del ModuleTestCase
